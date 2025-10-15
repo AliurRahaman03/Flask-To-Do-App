fa0275f8743c0e665fd0afb85feee59f7dfbdbf9
Aliur Rahaman <aliur474@gmaile.com>
2025-10-15 13:02:51 +0530

    2nda

--- app/models/models.py
+++ app/models/models.py
@@ -1,10 +1,3 @@
-'''from app import db
-
-class Task(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(120), nullable=False)
-    status = db.Column(db.String(40), default="Pending")'''
-
 from app import db
 
 class Task(db.Model):
