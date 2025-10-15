14ce70e3f469b334c260c56860e5fc31801a0085
Aliur Rahaman <aliur474@gmaile.com>
2025-10-15 12:48:08 +0530

    Initial commit - Flask project

---
+++ app/models/models.py
@@ -0,0 +1,13 @@
+'''from app import db
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    status = db.Column(db.String(40), default="Pending")'''
+
+from app import db
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    status = db.Column(db.String(40), default="Pending")
---
+++ app/routes/auth.py
@@ -0,0 +1,29 @@
+from flask import Blueprint, render_template, redirect, request, url_for, flash, session
+
+auth_bp = Blueprint('auth', __name__)
+
+USER_CREDENTIALS = {
+    "username": "admin",
+    "password": "12345"
+}
+
+@auth_bp.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        if username == USER_CREDENTIALS["username"] and password == USER_CREDENTIALS["password"]:
+            session["user"] = username
+            flash("Login Successful", "success")
+            # redirect to tasks (POST-Redirect-GET)
+            return redirect(url_for('tasks.view_tasks'))
+        else:
+            flash("Invalid username or password", "danger")
+    return render_template("login.html")
+
+@auth_bp.route("/logout")
+def logout():
+    session.pop("user", None)
+    flash("Loged out", "info")
+    return redirect(url_for("auth.login"))
