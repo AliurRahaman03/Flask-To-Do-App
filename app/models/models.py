c63c28f6d7e79d14a94adbaeb1848570319451a8
Aliur Rahaman <aliur474@gmaile.com>
2025-10-15 12:59:51 +0530

    2nd

--- app/models/models.py
+++ app/models/models.py
@@ -11,3 +11,8 @@ class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120), nullable=False)
     status = db.Column(db.String(40), default="Pending")
+    
+class UserModel():
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), nullable=False)
+    password = db.Column(db.String(120), nullavle=False)
